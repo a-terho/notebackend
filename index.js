baebcf00721aec9d94dfacbccd8303726218dccf
a-terho <140437240+a-terho@users.noreply.github.com>
2026-03-30 11:50:12 +0300

    Removed cors

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
 app.use(express.static('dist'));
 app.use(express.json());
-app.use(cors());
 
 const requestLogger = (req, response, next) => {
   console.log('Method:', req.method);
