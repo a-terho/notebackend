1683881d20f15baa876736a1cdd706a6d6405991
a-terho <140437240+a-terho@users.noreply.github.com>
2026-03-31 18:46:05 +0300

    autobuild

--- index.js
+++ index.js
@@ -1,123 +1,105 @@
-require('dotenv').config();
-const express = require('express');
-const Note = require('./models/note');
+require('dotenv').config()
+const express = require('express')
+const Note = require('./models/note')
 
-const app = express();
-app.use(express.static('dist'));
-app.use(express.json());
+const app = express()
+app.use(express.static('dist'))
+app.use(express.json())
 
 const requestLogger = (req, response, next) => {
-  console.log('Method:', req.method);
-  console.log('Path:  ', req.path);
-  console.log('Body:  ', req.body);
-  console.log('---');
-  next();
-};
-app.use(requestLogger);
-
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
+  console.log('Method:', req.method)
+  console.log('Path:  ', req.path)
+  console.log('Body:  ', req.body)
+  console.log('---')
+  next()
+}
+app.use(requestLogger)
 
 app.get('/', (req, res) => {
-  res.status(200).send('<h1>Hello World!</h1>');
-});
+  res.status(200).send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/notes', (req, res) => {
   Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
+    res.json(notes)
+  })
+})
 
 app.get('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   Note.findById(id)
     .then((note) => {
       if (note) {
-        return res.json(note);
+        return res.json(note)
       } else {
-        return res.status(404).end();
+        return res.status(404).end()
       }
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.put('/api/notes/:id', (req, res, next) => {
-  const { content, important } = req.body;
+  const { content, important } = req.body
 
   Note.findById(req.params.id)
     .then((note) => {
       if (!note) {
-        return res.status(404).end();
+        return res.status(404).end()
       }
 
-      note.content = content;
-      note.important = important;
+      note.content = content
+      note.important = important
 
       return note.save().then((updatedNote) => {
-        res.json(updatedNote);
-      });
+        res.json(updatedNote)
+      })
     })
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.delete('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
+  const id = req.params.id
   Note.findByIdAndDelete(id)
     .then(() => res.status(204).end())
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 app.post('/api/notes', (req, res, next) => {
-  const body = req.body;
+  const body = req.body
 
   if (!body.content) {
     return res.status(400).json({
       error: 'content missing',
-    });
+    })
   }
 
   const note = Note.create({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
-  note.then((savedNote) => res.json(savedNote)).catch((err) => next(err));
-});
+  note.then((savedNote) => res.json(savedNote)).catch((err) => next(err))
+})
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
-};
-app.use(unknownEndpoint);
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const errorHandler = (err, req, res, next) => {
-  console.error(err.message);
+  console.error(err.message)
 
   if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' });
+    return res.status(400).send({ error: 'malformatted id' })
   } else if (err.name === 'ValidationError') {
-    return res.status(400).json({ error: err.message });
+    return res.status(400).json({ error: err.message })
   }
 
-  next(err);
-};
-app.use(errorHandler);
+  next(err)
+}
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/note.js
+++ models/note.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI;
-mongoose.set('strictQuery', false);
+const url = process.env.MONGODB_URI
+mongoose.set('strictQuery', false)
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 mongoose
   .connect(url, { family: 4 })
   .then(() => console.log('connected to MongoDB'))
-  .catch((err) => console.log('error connecting to MongoDB:', err.message));
+  .catch((err) => console.log('error connecting to MongoDB:', err.message))
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -16,15 +16,15 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
+})
 
 // replace _id -> id as a string
 noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
